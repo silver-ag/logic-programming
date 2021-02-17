3c4dab248676cf2fece9586a93c95f6cfa550a58
Asa <asaglenister@protonmail.com>
2021-02-17 13:41:24 +0000

    added dependency on rackunit

--- info.rkt
+++ info.rkt
@@ -1,4 +1,5 @@
 #lang info
-(define deps '("base"))
-(define build-deps '("db-doc" "db-lib" "racket-doc" "scribble-lib"))
+
 (define scribblings '(("logic-programming.scrbl" ())))
+(define deps '("base" "rackunit-lib"))
+(define build-deps '("db-doc" "db-lib" "racket-doc" "scribble-lib"))
